7b55844b3cc13f87a7f3bd659cd6c56f181147a1
Dimidrol112 <dima.dalman@yandex.ru>
2018-08-16 18:02:24 +0500

    Initial commit.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public InputField sizeIf;
+    public GameObject cam;
+    public GameObject prefab;
+    public Tower[] towers;
+    public Transform[] bases;
+    public float speed = 10;
+    public int count = 5;
+    public int gap = 1;
+
+    public void Restart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Solve()
+    {
+        PrepScene();
+        StartCoroutine(MoveStack(count, 0, 2));
+    }
+    public void PrepScene()
+    {
+        count = int.Parse(sizeIf.text);
+        //Debug.Log(count);
+        towers[1].transform.Translate(new Vector3(count + gap, 0, 0));
+        towers[2].transform.Translate(new Vector3(count * 2 + gap * 2, 0, 0));
+
+        Tower.moveSpeed = speed;
+
+        foreach (var _base in bases)
+        {
+            _base.localScale = new Vector3(count, 1, count);
+        }
+
+        towers[0].GenTower(prefab, count);
+
+        cam.transform.SetPositionAndRotation(new Vector3(towers[1].transform.position.x, count, -count * 3), Quaternion.identity);
+
+
+    }
+
+    public void UpdateSize(float size)
+    {
+        sizeIf.text = size.ToString();
+    }
+
+    public void UpdateSpeed(float _speed)
+    {
+        speed = _speed;
+    }
+
+    public IEnumerator MoveStack(int size, int from, int to)
+    {
+        if (size <= 0)
+            yield break;
+
+        int third = ThirdAxe(from, to);
+
+        yield return MoveStack(size - 1, from, third);
+
+        //Debug.Log(from + " " + to);
+        yield return towers[from].MoveRing(towers[to]);
+
+        yield return MoveStack(size - 1, third, to);
+    }
+
+    public int ThirdAxe(int from, int to)
+    {
+        int result = 0;
+        switch (from)
+        {
+            case 0:
+                if (to == 1) { result = 2; } else { result = 1; }
+                break;
+            case 1:
+                if (to == 2) { result = 0; } else { result = 2; }
+                break;
+            case 2:
+                if (to == 0) { result = 1; } else { result = 0; }
+                break;
+        }
+        return result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public static float moveSpeed = 1;
+    public List<Ring> rings = new List<Ring>();
+
+    public void GenTower(GameObject prefab, int count)
+    {
+        for (int i = count; i > 0; i--)
+        {
+            var cube = Instantiate(prefab, transform.position, transform.rotation);
+            cube.transform.localScale = new Vector3(i, 1, i);
+            cube.transform.Translate(new Vector3(0, count - i, 0));
+            cube.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0, 1, 0, 1, 0, 1, 0.8f, 1);
+            rings.Add(new Ring(cube, i));
+        }
+    }
+
+    public IEnumerator MoveRing(Tower tow)
+    {
+        tow.rings.Add(rings[rings.Count - 1]);
+        rings.RemoveAt(rings.Count - 1);
+        Vector3 ringsPos = new Vector3(tow.transform.position.x, tow.rings.Count - 1, tow.transform.position.z);
+        yield return MoveTo(tow.rings[tow.rings.Count - 1].prefab.transform, ringsPos);
+    }
+
+    IEnumerator MoveTo(Transform trans, Vector3 pos)
+    {
+        while (Vector3.Distance(trans.position, pos) > 0.1f)
+        {
+            trans.position = Vector3.Lerp(trans.position, pos, Time.deltaTime * moveSpeed);
+            yield return null;
+        }
+    }
+}
+
+[Serializable]
+public class Ring
+{
+    public Ring(GameObject _prefab, int _size)
+    {
+        prefab = _prefab;
+        size = _size;
+    }
+    public GameObject prefab;
+    public int size = 1;
+}
